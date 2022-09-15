0ea62c9094409e56bfc112a0b461d6ae8e1489f3
Alvarotloc <alvarotloc@gmail.com>
2022-09-15 18:53:58 +0200

    Añadida funcionalidad de crear ticket desde el front

--- models/sockets.js
+++ models/sockets.js
@@ -1,12 +1,21 @@
+const TicketList = require("./ticket-list");
+
 class Sockets {
     constructor(io){
         this.io = io;
         this.socketEvents();
+
+        this.ticketList = new TicketList();
     }
 
     socketEvents(){
         this.io.on('connection',(socket) => {
             console.log('Usuario conectado');
+
+            socket.on('generar-ticket',(data,callback) => {
+                const nuevoTicket = this.ticketList.crearTicket();
+                callback(nuevoTicket);
+            })
         })
     }
 
--- models/ticket-list.js
+++ models/ticket-list.js
@@ -13,31 +13,29 @@ class TicketList {
     return this.ultimoNumero;
   }
 
-  get ultimosOcho(){
-    return this.asignados.slice(0,8);
+  get ultimosOcho() {
+    return this.asignados.slice(0, 8);
+  }
+
+  crearTicket() {
+    const nuevoTicket = new Ticket(this.siguienteNumero);
+    this.pendientes.push(nuevoTicket);
+    return nuevoTicket;
+  }
+
+  asignarTicket(agente, escritorio) {
+    if ([agente, escritorio].includes("")) return;
+    if (this.pendientes.length === 0) return;
+
+    const siguienteTicket = this.pendientes.shift();
+
+    siguienteTicket.agente = agente;
+    siguienteTicket.escritorio = escritorio;
+
+    this.asignados.unshift(siguienteTicket);
+
+    return siguienteTicket;
+  }
 }
-
-    crearTicket(){
-        const nuevoTicket = new Ticket(this.siguienteNumero);
-        this.pendientes.push(nuevoTicket);
-        return nuevoTicket;
-    }
-
-    asignarTicket(agente,escritorio){
-        if([agente,escritorio].includes('')) return;
-        if(this.pendientes.length === 0) return;
-
-        const siguienteTicket = this.pendientes.shift();
-
-        siguienteTicket.agente = agente;
-        siguienteTicket.escritorio = escritorio;
-
-        this.asignados.unshift(siguienteTicket);
-
-        return siguienteTicket
-    }
-
-}
-
 
 module.exports = TicketList;
